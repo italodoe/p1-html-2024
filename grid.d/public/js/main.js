2d3f74d9eabea39477f1115eea3383a1e63cb9f9
Italo Castillo <italogitdoe@gmail.com>
2024-03-15 00:21:03 +0100

    add mouse event to follow cursor

--- grid.d/public/js/main.js
+++ grid.d/public/js/main.js
@@ -2,11 +2,32 @@ function randomGenerateColor() {
   return [...Array(3).keys()].map(() => Math.floor(Math.random() * 256));
 }
 
-console.log(randomGenerateColor());
+function setRandomColorStyle(card) {
+  let randColor = randomGenerateColor();
+  return (card.style = `---bg-r: ${randColor[0]};---bg-g: ${randColor[1]};---bg-b: ${randColor[2]}`);
+}
 
 for (const card of document.querySelectorAll(".card")) {
   console.log(card);
-  let randColor = randomGenerateColor();
-  console.log(randColor);
-  card.style = `---bg-r: ${randColor[0]};---bg-g: ${randColor[1]};---bg-b: ${randColor[2]}`;
+  setRandomColorStyle(card);
 }
+
+window.addEventListener(
+  "mousemove",
+  function (e) {
+    offset = {
+      x: e.clientX,
+      y: e.clientY,
+    };
+
+    console.log(offset);
+    const target = e.target;
+    console.log(target);
+    const rect = target.getBoundingClientRect();
+    console.log(rect);
+    const x = offset.x - rect.left;
+    const y = offset.y - rect.top;
+    target.style.cssText += `---mouse-x: ${x}px; ---mouse-y: ${y}px;`
+  },
+  true
+);
