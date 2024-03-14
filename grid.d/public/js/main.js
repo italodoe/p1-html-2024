1396906feefd192a139f3d147b95ef35bc7c9d07
Italo Castillo <italogitdoe@gmail.com>
2024-03-14 23:59:52 +0100

    Add random color

--- grid.d/public/js/main.js
+++ grid.d/public/js/main.js
@@ -1,1 +1,12 @@
-console.log('hey')
+function randomGenerateColor() {
+  return [...Array(3).keys()].map(() => Math.floor(Math.random() * 256));
+}
+
+console.log(randomGenerateColor());
+
+for (const card of document.querySelectorAll(".card")) {
+  console.log(card);
+  let randColor = randomGenerateColor();
+  console.log(randColor);
+  card.style = `---bg-r: ${randColor[0]};---bg-g: ${randColor[1]};---bg-b: ${randColor[2]}`;
+}
