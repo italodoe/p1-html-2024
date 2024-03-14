090fdc01d5df967f7e0302a1ea0cf4531b4fe41a
Italo Castillo <italogitdoe@gmail.com>
2024-03-14 01:43:40 +0100

    drag along the X axis

--- experiment.d/public/js/main.js
+++ experiment.d/public/js/main.js
@@ -1,1 +1,40 @@
-console.log('works');
+var mousePosition;
+var offset = {x: 0, y:0};
+var isDown = false;
+
+const track_elem = document.getElementById("track");
+
+track_elem.addEventListener(
+  "mousedown",
+  function (e) {
+    isDown = true;
+    offset = {
+        x: track_elem.offsetLeft - e.clientX, 
+        y: track_elem.offsetTop - e.clientY
+    };
+  },
+  true
+);
+
+document.addEventListener(
+  "mouseup",
+  function () {
+    isDown = false;
+  },
+  true
+);
+
+document.addEventListener(
+  "mousemove",
+  function (event) {
+    event.preventDefault();
+    if (isDown) {
+      mousePosition = {
+        x: event.clientX,
+        y: event.clientY,
+      };
+      track_elem.style.left = mousePosition.x + offset.x + "px";
+    }
+  },
+  true
+);
