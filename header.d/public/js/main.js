f33abf71e79991b91ab61b4a17f7c1046537aafd
Italo Castillo <italogitdoe@gmail.com>
2024-03-15 20:43:48 +0100

    hide layer

--- header.d/public/js/main.js
+++ header.d/public/js/main.js
@@ -1,1 +1,38 @@
- 
+const glow_elem = document.getElementById("glow");
+
+window.addEventListener(
+  "mousedown",
+  function (e) {
+    isDown = true;
+    offset = {
+      x: track_elem.offsetLeft - e.clientX,
+      y: track_elem.offsetTop - e.clientY,
+    };
+  },
+  true
+);
+
+window.addEventListener(
+  "mouseup",
+  function (e) {
+    isDown = false;
+  },
+  true
+);
+
+window.addEventListener(
+  "mousemove",
+  function (event) {
+    event.preventDefault();
+    
+    mousePosition = {
+      x: event.clientX,
+      y: event.clientY,
+    };
+    glow_elem.style.transform = `translate(${mousePosition.x - 300}px, ${
+      mousePosition.y - 300
+    }px)`;
+
+  },
+  true
+);
