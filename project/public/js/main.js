7c8be1bd28551fc0d7448e1b00a73ce8a7674beb
Italo Castillo <italogitdoe@gmail.com>
2024-03-16 02:48:08 +0100

    add glow to header

---
+++ project/public/js/main.js
@@ -0,0 +1,29 @@
+function randomGenerateColor() {
+  return [...Array(3).keys()].map(() => Math.floor(Math.random() * 256));
+}
+
+function setRandomColorStyle(card) {
+  let randColor = randomGenerateColor();
+  return (card.style = `---bg-r: ${randColor[0]};---bg-g: ${randColor[1]};---bg-b: ${randColor[2]}`);
+}
+
+for (const card of document.querySelectorAll(".card-h")) {
+  console.log(card);
+  setRandomColorStyle(card);
+}
+
+window.addEventListener(
+  "mousemove",
+  function (e) {
+    offset = {
+      x: e.clientX,
+      y: e.clientY,
+    };
+    const target = e.target;
+    const rect = target.getBoundingClientRect();
+    const x = offset.x - rect.left;
+    const y = offset.y - rect.top;
+    target.style.cssText += `---mouse-x: ${x}px; ---mouse-y: ${y}px;`;
+  },
+  true
+);
